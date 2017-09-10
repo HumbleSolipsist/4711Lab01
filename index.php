a78cdfe65d3033b64863124d0c5888290128e6ab
Andrew Busto <aBusto@shaw.ca>
2017-09-10 00:13:24 -0700

    code commented

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -13,8 +8,10 @@ and open the template in the editor.
         <?php
         include('Student.php');
 
+        // Will contain 3 students to be displayed.
         $students = array();
         
+        // First student added to $students.  Named "John Doe."
         $first = new Student();
         $first->surname = "Doe";
         $first->first_name = "John";
@@ -25,6 +22,7 @@ and open the template in the editor.
         $first->add_grade(55);
         $students['j123'] = $first;
         
+        // Second student added to $students.  Named "Alber Einstein."
         $second = new Student();
         $second->surname = "Einstein";
         $second->first_name = "Albert";
@@ -35,7 +33,19 @@ and open the template in the editor.
         $second->add_grade(80);
         $second->add_grade(50);
         $students['a456'] = $second;
+        
+        // Third student added to $students.  Named "Andrew Busto."
+        $andrew = new Student();
+        $andrew->surname = "Busto";
+        $andrew->first_name = "Andrew";
+        $andrew->add_email('only', 'andrew@andrew.andrew');
+        $andrew->add_grade(25);
+        $andrew->add_grade(90);
+        $andrew->add_grade(80);
+        $students['a234'] = $andrew;
 
+        ksort($students);
+        
         foreach($students as $student)
             echo $student->toString();
         ?>

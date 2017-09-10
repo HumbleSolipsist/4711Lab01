4906976820f4a3362a971ea5efc47b058d2236a3
Andrew Busto <aBusto@shaw.ca>
2017-09-09 23:13:52 -0700

    Student class actually has stuff now

--- Student.php
+++ Student.php
@@ -12,5 +12,18 @@
  * @author Andrew
  */
 class Student {
-    //put your code here
+    function __construct() {
+        $this->surname = '';
+        $this->first_name = '';
+        $this->emails = array();
+        $this->grades = array();
+    }
+    
+    function add_email($which,$address) {
+        $this->emails[$which] = $address;
+    }
+
+    function add_grade($grade) {
+        $this->grades[] = $grade;
+    }
 }

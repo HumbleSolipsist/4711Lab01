fd09924e05ac988c92ee4707ed4759d6b2d9768b
Andrew Busto <aBusto@shaw.ca>
2017-09-10 00:13:13 -0700

    code commented

--- Student.php
+++ Student.php
@@ -1,17 +1,13 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of Student
+ * Contains data pertaining to a single Student.
  *
  * @author Andrew
  */
 class Student {
+    /**
+     * Constructor for Student.
+     */
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -19,14 +15,30 @@ class Student {
         $this->grades = array();
     }
     
+    /**
+     * Adds a given e-mail to this Student's list of e-mails at the given key.
+     * 
+     * @param type $which The key associated with the given e-mail address.
+     * @param type $address The e-mail address to be added to the list.
+     */
     function add_email($which,$address) {
         $this->emails[$which] = $address;
     }
 
+    /**
+     * Appends a given grade to this Student's list of grades.
+     * 
+     * @param type $grade The grade to be appended to the list.
+     */
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
+    /**
+     * Gets the average of all this Student's grades.
+     * 
+     * @return The average of all this Student's grades.
+     */
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -34,6 +46,11 @@ class Student {
         return $total / count($this->grades);
     }
     
+    /**
+     * Gets a String representation of this Student.
+     * 
+     * @return A String representation of this Student.
+     */
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().")\n";
